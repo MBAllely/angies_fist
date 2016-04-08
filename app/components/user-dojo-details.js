b6965852ac1f958e7bd9957691597baf6c98d22c
Marika <mballely@gmail.com>
2016-04-08 15:42:34 -0700

    add computed address property

--- app/components/user-dojo-details.js
+++ app/components/user-dojo-details.js
@@ -1,6 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  address: Ember.computed('street', 'state', 'zip', function() {
+    return this.get('dojo.street') + " " + this.get('dojo.state') + "\n " + this.get('dojo.zip');
+  }),
+
+  
+
   favorites: Ember.inject.service(),
 
   actions: {
