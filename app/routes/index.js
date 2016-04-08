75029b210db5dc61324b20778d509be2d7dbe8e5
Marika <mballely@gmail.com>
2016-04-08 11:08:03 -0700

    add reviews to index model hash

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    return this.store.findAll('dojo');
+  model(params) {
+    return Ember.RSVP.hash({
+      dojos: this.store.findAll('dojo'),
+      reviews: this.store.findAll('review')
+    });
   }
 });
