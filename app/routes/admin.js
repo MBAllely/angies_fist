58a6fa42d3868c69daedc5dad92f876f32e8306d
Marika <mballely@gmail.com>
2016-04-08 08:59:22 -0700

    add addDojoForm to admin page

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -2,6 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    
+    return this.store.findAll('dojo');
+  },
+  actions: {
+    addDojo(params) {
+      var newDojo = this.store.createRecord('dojo', params);
+      newDojo.save();
+      this.transitionTo('admin');
+    }
   }
 });
