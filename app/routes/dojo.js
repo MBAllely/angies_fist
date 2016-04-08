b4067464b484e03605bf89e865e961746fd5f3c7
Marika <mballely@gmail.com>
2016-04-08 12:08:06 -0700

    fix display individual dojos

--- app/routes/dojo.js
+++ app/routes/dojo.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params) {
+    return Ember.RSVP.hash({
+      dojos: this.store.findRecord('dojo', params.dojo_id),
+      reviews: this.store.findAll('review')
+    });
+  }
 });
