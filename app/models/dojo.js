544f66a4f600d76856d37436819a37f7c1be87be
Marika <mballely@gmail.com>
2016-04-08 08:44:34 -0700

    add addDojoForm

--- app/models/dojo.js
+++ app/models/dojo.js
@@ -2,7 +2,9 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  location: DS.attr('string'),
+  street: DS.attr('string'),
+  state: DS.attr('string'),
+  zip: DS.attr('number'),
   phone: DS.attr('number'),
   image: DS.attr('string'),
   reviews: DS.hasMany('review', { async: true })
