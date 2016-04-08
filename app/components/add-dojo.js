21905e4a0347dd9e92828429dd56859915c1a562
Marika <mballely@gmail.com>
2016-04-08 09:08:58 -0700

    display dojos on admin side

--- app/components/add-dojo.js
+++ app/components/add-dojo.js
@@ -12,7 +12,7 @@ export default Ember.Component.extend({
         street: this.get('street') ? this.get('street') : "Jackie Chan st",
         state: this.get('state') ? this.get('state') : "OR",
         zip: this.get('zip') ? this.get('zip') : "97219",
-        phone: this.get('phone') ? this.get('phone') : 907-555-4433,
+        phone: this.get('phone') ? this.get('phone') : 9075554433,
         image: this.get('image') ? this.get('image') : "https://s-media-cache-ak0.pinimg.com/236x/c9/a2/6d/c9a26d17b407b5768c995ce27a0adb81.jpg",
       };
       this.sendAction('addDojo', params);
